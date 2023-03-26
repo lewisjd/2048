0947dc691ead80636b6d11564e4a38b31395b153
Lewis <lewisdunning3@gmail.com>
2023-03-26 14:54:57 +0100

    colours

--- javascript/script.js
+++ javascript/script.js
@@ -76,7 +76,32 @@ function updateUI() {
 
 // Get the background color for a specific cell value
 function getCellBackgroundColor(value) {
-    // Return the corresponding color based on the cell value
+    switch (value) {
+        case 2:
+            return "#eee4da";
+        case 4:
+            return "#ede0c8";
+        case 8:
+            return "#f2b179";
+        case 16:
+            return "#f59563";
+        case 32:
+            return "#f67c5f";
+        case 64:
+            return "#f65e3b";
+        case 128:
+            return "#edcf72";
+        case 256:
+            return "#edcc61";
+        case 512:
+            return "#edc850";
+        case 1024:
+            return "#edc53f";
+        case 2048:
+            return "#edc22e";
+        default:
+            return "#ccc0b3";
+    }
 }
 
 // Initialize the game board with empty cells
